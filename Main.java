d3efc9fc25085e49c15cd05e1387db9a9814596d
LuterGS <koo04034@gmail.com>
2018-04-10 18:46:12 +0900

    변경

--- Main.java
+++ Main.java
@@ -1,2 +1,26 @@
+import java.util.Random;
+
 public class Main {
+
+    public static void main(String[] args){
+
+        int select;
+        Random random = new Random();
+
+        Player_1st Warrior = new Player_1st("강지원");
+
+        while(true) {
+            select = random.nextInt(100);
+
+            if(select >= 0 && select <= 29){
+                //전투
+            }else if(select >= 30 && select <= 40){
+
+            }
+
+
+
+
+        }
+    }
 }
--- Weapon.java
+++ Weapon.java
@@ -4,17 +4,12 @@ class Weapon{
 
     private Random random = new Random();
     private String name;
-    protected int ATK, DEF, Tier;
+    protected int ATK, DEF
     private Object weapon_property = null;
 
     public Weapon(){
-    }
-
-    public Weapon(int Tier){
-        this.Tier = Tier;
-        this.ATK = (random.nextInt(15) + 25) + (this.Tier * 5);
-        this.DEF = (random.nextInt(15) + 25) + (this.Tier * 5);
-        set_property();
+        this.ATK = (random.nextInt(15) + 25));
+        this.DEF = (random.nextInt(15) + 25));
 
         if(this.ATK > this.DEF){
             this.name = Events.set_name("무기");
@@ -25,19 +20,7 @@ class Weapon{
             this.name = Events.set_name("무기");
         }
     }
-    //Weapon 기본 생성자
 
-    private void set_property(){
-
-        int num = random.nextInt(3);
-        if (num == 0) {
-            weapon_property = new Fire();
-        }else if(num == 1) {
-            weapon_property = new Ice();
-        }else if(num == 2){
-            weapon_property = new Dark();
-        }
-    }
     //Weapon 속성 생성 메소드, Weapon 생성자에서 기본 설정됨
 
     public String get_name(){
@@ -51,11 +34,9 @@ class Weapon{
         else if(num == 2){
             return this.DEF;
         }
-        else if(num == 3){
-            return this.Tier;
-        }else{
+        else{
             return 0;
         }
     }
-    //getter 확장 구현, 일일히 구현하는 것보다 번호를 이용해 한번에 여러 Value를 return 하게 함
+
 }
